a3c9da5abbea68dd03002527d8e961107061b289
79087 <footba79ll@mail.ru>
2023-11-14 22:58:02 +0300

    Initial commit

---
+++ recruto_test/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RecrutoTestConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'recruto_test'
---
+++ recruto_test/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    name = request.GET.get('name', '')
+    message = request.GET.get('message', '')
+    return HttpResponse(f"Hello {name}! {message}!")
