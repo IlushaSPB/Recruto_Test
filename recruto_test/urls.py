1a6b700147b932f16170e062db7f0241c082d8df
79087 <footba79ll@mail.ru>
2023-11-17 13:55:36 +0300

    update

--- recruto_test/urls.py
+++ recruto_test/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.index),
+    path('auth_random/', views.auth_random, name='auth_random'),
+    path('', views.random_code),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
--- recruto_test/views.py
+++ recruto_test/views.py
@@ -1,7 +1,31 @@
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+import random
+from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect
 
 
-def index(request):
-    name = request.GET.get('name', '')
-    message = request.GET.get('message', '')
-    return HttpResponse(f"Hello {name}! {message}!")
+# def index(request):
+#     name = request.GET.get('name', '')
+#     message = request.GET.get('message', '')
+#     return HttpResponse(f"Hello {name}! {message}!")
+
+
+def random_code(request):
+    code = str(random.randint(1000, 9999))
+    return render(request, 'random.html', {'code': code})
+
+
+@login_required
+def auth_random(request):
+    if request.method == 'GET':
+        logout(request)
+    code = str(random.randint(1000, 9999))
+    request.session['code'] = code
+
+    return render(request, 'auth_random.html', {'code': request.session['code']})
+
+# @login_required
+# def update_auth_random(request):
+#     request.session['code'] = str(random.randint(1000, 9999))
+#     return redirect('login')
+
